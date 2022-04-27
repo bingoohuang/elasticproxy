ce16e31a1e653f8922ff42ccfa6d202deeba4eb3
bingoohuang <bingoo.huang@gmail.com>
2022-04-27 17:30:27 +0800

    backups

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# ElasticProxy
+# elasticproxy
 
-This little program acts as a read-only proxy for ElasticSearch. It is intended to be used as
-ElasticSearch server for Kibana, ensuring that Kibana can only perform GET calls on ElasticSearch.
+This little program acts as a proxy for ElasticSearch.
+
+![image](elasticproxy.svg)
 
 1. build: `make install`
-2. initialize: `elasticproxy -init`
-3. start: `./ctl start`
-4. tail log: `./ctl tail`
+2. initialize: `elasticproxy -init` (this will create ctl shell script for convenience of startup or stop)
+3. start [httplive](https://github.com/bingoohuang/httplive), for backup mocking: `httplive -l`
+   , [download httplive](http://7.d5k.co/httplive/dl/)
+4. start: `./ctl start -b http://127.0.0.1:5003/backup`
+5. tail log: `./ctl tail`
 
 ## usage
 
@@ -23,14 +26,17 @@ Usage of elasticproxy:
 
 ```sh
 $ ./ctl tail    
-2022-04-27 16:34:15.091 [INFO ] 15152 --- [1    ] [-]  : log file created:~/logs/elasticproxy/elasticproxy.log
-2022-04-27 16:35:52.730 [INFO ] 15152 --- [18   ] [-]  : {"duration":"25.620513ms","method":"POST","path":"/person/doc","remote_addr":"127.0.0.1:59931","status":201}
-2022-04-27 16:36:47.631 [INFO ] 15152 --- [34   ] [-]  : {"duration":"40.725229ms","method":"GET","path":"/_search","remote_addr":"127.0.0.1:60132","status":200}
-2022-04-27 16:36:48.274 [INFO ] 15152 --- [34   ] [-]  : {"duration":"7.867817ms","method":"GET","path":"/favicon.ico","remote_addr":"127.0.0.1:60132","status":200}
+2022-04-27 17:07:23.604 [INFO ] 16843 --- [1    ] [-]  : log file created:~/logs/elasticproxy/elasticproxy.log
+2022-04-27 17:07:31.155 [INFO ] 16843 --- [23   ] [-]  : {"direction":"primary","duration":"29.512085ms","method":"POST","path":"/person/doc","remote_addr":"127.0.0.1:50693","status":201,"target":"http://127.0.0.1:9200/person/doc"}
+2022-04-27 17:07:31.157 [INFO ] 16843 --- [22   ] [-]  : {"direction":"backup","duration":"2.130054ms","status":200,"target":"http://127.0.0.1:5003/backup/person/doc"}
+2022-04-27 17:07:42.886 [INFO ] 16843 --- [34   ] [-]  : {"direction":"primary","duration":"21.780433ms","method":"GET","path":"/_search","remote_addr":"127.0.0.1:50736","status":200,"target":"http://127.0.0.1:9200/_search"}
+2022-04-27 17:07:43.374 [INFO ] 16843 --- [34   ] [-]  : {"direction":"primary","duration":"3.710797ms","method":"GET","path":"/favicon.ico","remote_addr":"127.0.0.1:50736","status":200,"target":"http://127.0.0.1:9200/favicon.ico"}
 ```
 
 ## help commands
 
 1. `docker-compose up && docker-compose rm -fsv`
 1. `jj -gu 'name=@姓名' 'sex=@random(男,女)' 'addr=@地址' 'idcard=@身份证' | gurl POST :2900/person/doc`
+   , [download jj](http://7.d5k.co/httplive/dl/)
 1. `gurl http://127.0.0.1:2900/_search` vs `http://127.0.0.1:9200/_search`
+   , [download gurl](http://7.d5k.co/httplive/dl/)
--- proxy.go
+++ proxy.go
@@ -16,35 +16,54 @@ import (
 
 // ElasticProxy forwards received HTTP calls to another HTTP server.
 type ElasticProxy struct {
-	primary *url.URL
-	backups []*url.URL
+	primary       *url.URL
+	backups       []*url.URL
+	backupReqChan chan backupReq
+}
+
+type backupReq struct {
+	body []byte
+	req  *http.Request
 }
 
 // CreateElasticProxy returns a new elasticProxy object.
 func CreateElasticProxy(elasticURL *url.URL, backups []*url.URL) *ElasticProxy {
-	return &ElasticProxy{
+	p := &ElasticProxy{
 		primary: elasticURL,
 		backups: backups,
 	}
+
+	if len(p.backups) > 0 {
+		p.backupReqChan = make(chan backupReq)
+		go p.backupProcess()
+	}
+
+	return p
 }
 
 // ServeHTTP only forwards allowed requests to the real ElasticSearch server.
 func (p *ElasticProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	status := 0
+	status := 200
+	target := JoinURL(p.primary, r.RequestURI)
 	fields := map[string]interface{}{
 		"remote_addr": r.RemoteAddr,
 		"method":      r.Method,
 		"path":        r.URL.Path,
+		"target":      target,
+		"direction":   "primary",
 	}
 	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
 		fields["x_forwarded_for"] = xff
 	}
 
 	startTime := time.Now()
-
+	headerWrote := false
 	defer func() {
 		fields["duration"] = time.Now().Sub(startTime).String()
 		fields["status"] = status
+		if !headerWrote {
+			w.WriteHeader(status)
+		}
 
 		accessLog, _ := ginx.JsoniConfig.MarshalToString(context.Background(), fields)
 		log.Print(accessLog)
@@ -59,22 +78,29 @@ func (p *ElasticProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		status = http.StatusInternalServerError
-		w.WriteHeader(status)
 		return
 	}
 
-	targetURL := *p.primary
-	targetURL.Path = path.Join(targetURL.Path, r.RequestURI)
-	targetURLAddr := targetURL.String()
-
-	req, err := http.NewRequest(r.Method, targetURLAddr, ioutil.NopCloser(bytes.NewBuffer(body)))
+	req, err := http.NewRequest(r.Method, target, ioutil.NopCloser(bytes.NewBuffer(body)))
 	req.Header = r.Header
 	rsp, err := client.Do(req)
+	if err != nil {
+		log.Printf("client do failed: %v", err)
+		status = http.StatusInternalServerError
+		return
+	}
+
+	status = rsp.StatusCode
+	if status >= 200 && status < 300 && r.Method == http.MethodPost && p.backupReqChan != nil {
+		p.backupReqChan <- backupReq{req: r, body: body}
+	}
 
 	data, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
 		log.Printf("reading response body failed: %v", err)
 	}
+
+	status = rsp.StatusCode
 	for k, vv := range rsp.Header {
 		for _, v := range vv {
 			w.Header().Add(k, v)
@@ -85,13 +111,61 @@ func (p *ElasticProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add(ContentLengthKey, fmt.Sprintf("%d", rsp.ContentLength))
 	}
 
-	status = rsp.StatusCode
 	w.WriteHeader(rsp.StatusCode)
+	headerWrote = true
 	if _, err := w.Write(data); err != nil {
 		log.Printf("write data failed: %v", err)
+	}
+}
+
+func (p *ElasticProxy) backupProcess() {
+	for req := range p.backupReqChan {
+		p.doBackup(req)
+	}
+}
+
+func (p *ElasticProxy) doBackup(req backupReq) {
+	for _, backup := range p.backups {
+		p.doBackupOne(req, backup)
+	}
+}
+
+func (p *ElasticProxy) doBackupOne(b backupReq, backup *url.URL) {
+	status := 0
+	target := JoinURL(backup, b.req.RequestURI)
+	fields := map[string]interface{}{
+		"direction": "backup",
+		"target":    target,
+	}
+	startTime := time.Now()
+
+	defer func() {
+		fields["duration"] = time.Now().Sub(startTime).String()
+		fields["status"] = status
+
+		accessLog, _ := ginx.JsoniConfig.MarshalToString(context.Background(), fields)
+		log.Print(accessLog)
+	}()
+
+	req, err := http.NewRequest(b.req.Method, target, ioutil.NopCloser(bytes.NewBuffer(b.body)))
+	req.Header = b.req.Header
+	rsp, err := client.Do(req)
+	if err != nil {
+		log.Printf("client do failed: %v", err)
+		return
+	}
+	status = rsp.StatusCode
+	if rsp.Body != nil {
+		_, _ = io.Copy(ioutil.Discard, rsp.Body)
 	}
 }
 
 const ContentLengthKey = "Content-Length"
 
 var client = &http.Client{}
+
+func JoinURL(base *url.URL, requestURI string) string {
+	targetURL := *base
+	targetURL.Path = path.Join(targetURL.Path, requestURI)
+	return targetURL.String()
+}
